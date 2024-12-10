9ac83033244ff31b6e73ee7cab2fb366f9860931
digbanmije18@gmail.com <digbanmije18@gmail.com>
2024-12-10 21:57:32 +0100

    object method

--- practice.js
+++ practice.js
@@ -470,9 +470,34 @@ const mark = {
   fullName: "Mark Miller",
   mass: 78,
   height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
 };
 const john = {
   fullName: "Mark Miller",
   mass: 92,
   height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
 };
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s bmi (${mark.bmi}) is higher than ${john.fullName}'s bmi(${john.bmi})`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s bmi (${john.bmi}) is higher than ${mark.fullName}'s bmi(${mark.bmi})`
+  );
+}
